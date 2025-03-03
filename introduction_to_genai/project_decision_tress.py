8863b3257e49e729da6c3b8e5ee240eba35262bc
BekzodM <bmusayev07@gmail.com>
2025-03-03 01:21:58 -0500

    Finish ljublana weather condition decision tree project.

--- introduction_to_genai/project_decision_tress.py
+++ introduction_to_genai/project_decision_tress.py
@@ -5,8 +5,42 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import StandardScaler
 
+#Reading a dataset downloaded from https://www.kaggle.com/datasets/thedevastator/weather-prediction/data
 df = pd.read_csv('weather_prediction_dataset.csv')
 
-cities = set(col.split('_')[0] for col in df.columns if '_' in col)
-first_city = cities[0]
-print(cities)
+#Creating a weather class to determine whether it is sunny or cloudy based on cloud cover in Ljublana, Slovenia
+df['Weather_Class'] = df['LJUBLJANA_cloud_cover'].apply(lambda x: 1 if x >= 4 else 0)
+print(df['Weather_Class'].value_counts())
+
+#features used to train and test our ML model
+features = ['LJUBLJANA_humidity', 'LJUBLJANA_pressure', 'LJUBLJANA_wind_speed',
+            'LJUBLJANA_global_radiation', 'LJUBLJANA_precipitation',
+            'LJUBLJANA_temp_mean', 'LJUBLJANA_temp_min', 'LJUBLJANA_temp_max']
+
+target = 'Weather_Class'
+
+X = df[features]
+y = df[target]
+
+#if we are missing any data
+df.fillna(df.mean(numeric_only=True), inplace=True)
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+#Setting train test split of 80/20
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=17, stratify=y)
+
+
+# Train Decision Tree Regressor
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+#Printing accuracy and report
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Model Accuracy: {accuracy:.2f}")
+print("Classification Report:\n", classification_report(y_test, y_pred))
+
+
