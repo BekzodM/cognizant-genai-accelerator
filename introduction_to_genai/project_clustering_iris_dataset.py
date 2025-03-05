a00990e19229865de6c115ea4ab4a2d9b3c522a5
BekzodM <bmusayev07@gmail.com>
2025-03-05 02:14:01 -0500

    Finish clustering project.

---
+++ introduction_to_genai/project_clustering_iris_dataset.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+df = pd.read_csv("iris.data.txt", delimiter=",", header=None)
+#setting class names provided in project description
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+df.columns = column_names
+#dropping NA values
+df.dropna(inplace=True)
+scaler = StandardScaler()
+#Adding standardized values to new columns
+df[['sepal_length_t', 'sepal_width_t', 'petal_length_t', 'petal_width_t']] = scaler.fit_transform(df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
+#dropping class column since not used in k-means algorithm
+X = df[['sepal_length_t', 'sepal_width_t', 'petal_length_t', 'petal_width_t']]
+
+#FINDING OPTIMAL K
+wcss = []
+
+for k in range(1, 10):
+    kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+""" plt.figure(figsize=(8,5))
+plt.plot(range(1, 10), wcss)
+plt.xlabel("Number of Clusters (k)")
+plt.ylabel("WCSS (Within-Cluster Sum of Squares)")
+plt.title("Elbow Method to Find Optimal k")
+plt.xticks(range(1, 10))
+plt.grid()
+plt.show() """
+
+#APPLYING K MEANS Clustering
+kmeans =  KMeans(n_clusters=3)
+kmeans.fit(X)
+df['kmeans_3'] = kmeans.labels_
+#save as csv to check accuracy of clusters
+df.to_csv("iris_result.csv", index=False)
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+clusters = kmeans.fit_predict(X_pca)
+plt.figure(figsize=(8,5))
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=clusters, cmap='viridis', edgecolors='k', alpha=0.7)
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+plt.title("K-Means Clustering (PCA Reduced to 2D)")
+plt.show()
